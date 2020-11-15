d03d036dcb68b89861f7c3fbc045dd6451bb12d6
Gilibeth Vega <gvega@creditu.com>
2020-11-15 18:18:59 -0300

    se completa desafío 2

--- desafiotop100.sql
+++ desafiotop100.sql
@@ -37,6 +37,36 @@ WHERE id IN
 --6. Listar los 10 directores mas populares, indicando su nombre y cuántas películas aparecen en el top 100.(1 puntos)
 SELECT director, count(*)
 FROM peliculastop100
-GROUp BY director
+GROUP BY director
 ORDER BY count DESC 
 LIMIT 10;
+-- 7. Indicar cuantos actores distintos hay (1 puntos)
+SELECT COUNT(numero_de_actores) FROM (
+  SELECT actor
+  FROM reparto
+  GROUP BY actor) 
+  AS numero_de_actores;
+--8. Indicar las películas estrenadas entre los años 1990 y 1999 (ambos incluidos) ordenadas por título de manera ascendente.(1 punto)
+SELECT pelicula_nombre
+FROM peliculastop100
+WHERE (year_estreno > 1989 AND year_estreno < 2000)
+ORDER BY pelicula_nombre ASC;
+--9. Listar el reparto de las películas lanzadas el año 2001 (1 punto)
+SELECT actor AS Reparto_Peliculas_2001
+FROM reparto
+WHERE id_actores IN (
+  SELECT id
+  FROM peliculastop100
+  WHERE year_estreno = 2001
+  );
+--10. Listar los actores de la película más nueva (1 punto)
+SELECT actor AS Reparto_Pelicula_Mas_Nueva
+FROM reparto
+WHERE id_actores IN (
+  SELECT id
+  FROM peliculastop100
+  ORDER BY year_estreno DESC
+  LIMIT 1
+  );
+
+
